d2b10b797b411d80fa53fc0962888b8e8043b5fa
Eugene <josumkin@yahoo.com>
2024-04-16 15:47:00 +0400

    feat: base interface added

---
+++ index.js
@@ -0,0 +1,43 @@
+import readline from 'readline';
+
+const userArgs = process.argv.slice(2);
+
+if (userArgs.length < 3 || userArgs.length % 2 === 0) {
+  console.error(`\x1b[31mPlease pass an odd number of arguments greater than or equal to 3.\x1b[0m
+For example: \x1b[33mnpm run start rock paper scissors\x1b[0m\n`);
+  process.exit(0);
+}
+
+let difficult = userArgs.length;
+
+console.log(`\x1b[36mWelcome to the Game!\x1b[0m\n`);
+
+async function mainLoop() {
+  try {
+    const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+    });
+
+    let keepGoing = true;
+    while (keepGoing) {
+    const userInput = await new Promise((resolve) => {
+        rl.question('Enter your input: ', (input) => {
+        resolve(input);
+        });
+    });
+
+    console.log(`You entered: ${userInput}`);
+
+    if (userInput === 'stop') {
+        keepGoing = false;
+        rl.close();
+        process.exit(0);
+    }
+    }
+  } catch (error) {
+      console.error('Error:', error);
+  }
+}
+
+mainLoop();
